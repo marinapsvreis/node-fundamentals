464505def8ce12dd367f6c5c61d1615d1367133f
Marina Valente <marinapsvreis@gmail.com>
2024-01-03 20:05:13 -0300

    feat: streams on http server

---
+++ streams/fake-upload-to-http-stream.js
@@ -0,0 +1,24 @@
+import { Readable } from 'node:stream'
+import fetch from 'node-fetch';
+
+class OneToHundredStream extends Readable {
+  index = 1
+
+  _read() {
+    const i = this.index++
+
+    setTimeout(() => {
+      if (i > 100) {
+        this.push(null)
+      } else {
+        const buf = Buffer.from(`${i}\n`)
+        this.push(buf)
+      }
+    }, 1000)
+  }
+}
+
+fetch('http://localhost:3334', {
+  method: 'POST',
+  body: new OneToHundredStream(),
+})
