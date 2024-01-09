07bd57b74f1f218bda0f2714cc90f0fb05861273
Marina Valente <marinapsvreis@gmail.com>
2024-01-08 22:19:36 -0300

    feat: deleting registers and configuring regex to identify route params

---
+++ src/routes.js
@@ -0,0 +1,44 @@
+import { randomUUID } from 'node:crypto';
+import { Database } from './database.js';
+import { buildRoutePath } from './utils/build-route-path.js';
+
+const database = new Database(); 
+
+export const routes = [
+  {
+    method: 'GET',
+    path: buildRoutePath('/users'),
+    handler: (request, response) => {
+      const users = database.select('users');
+      return response.end(JSON.stringify(users));
+    }
+  },
+  {
+    method: 'POST',
+    path: buildRoutePath('/users'),
+    handler: (request, response) => {
+      const { name, email } = request.body;
+
+      const user = {
+        id: randomUUID(),
+        name,
+        email,
+      };
+
+      database.insert('users', user);
+
+      return response.writeHead(201).end('Usuário criado');
+    }
+  },
+  {
+    method: 'DELETE',
+    path: buildRoutePath('/users/:id'),
+    handler: (request, response) => {
+      const id = request.params.id;
+
+      database.delete('users', id);
+
+      return response.writeHead(204).end();
+    }
+  }
+];
